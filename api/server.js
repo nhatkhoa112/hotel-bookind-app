07c0c799c17e3d858a6e55cc551305b62b17de59
Nhat Khoa <nhatkhoa@Nhats-MacBook-Air.local>
2022-07-19 20:19:55 +0700

    create the backend

--- api/server.js
+++ api/server.js
@@ -1,17 +1,23 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-dotenv.config();
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
 
+
+const authRoute = require('./routes/auth');
+const usersRoute = require('./routes/users');
+const hotelsRoute = require('./routes/hotels');
+const roomsRoute = require('./routes/rooms') 
+
+
 //middlewares
-app.use(cors())
-app.use(cookieParser())
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 
 mongoose
@@ -26,6 +32,21 @@ mongoose
     console.log(err.message);
   });
 
+  app.use('./api', authRoute);
+  app.use('./api/user', usersRoute);
+  app.use('./api/room', usersRoute);
+  app.use('./api/hotel', hotelsRoute)
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
+    });
+  });
 
 const server = app.listen(process.env.PORT, () =>
 {
